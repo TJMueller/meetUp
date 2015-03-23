7ad65e378b0d1e5dd9c76413a68ec40e44955463
TJMueller <timothyjonmueller@gmail.com>
2015-03-23 13:45:43 -0500

    Initial Commit

---
+++ MeetUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetUp
+//
+//  Created by Timothy Mueller on 3/23/15.
+//  Copyright (c) 2015 Timothy Mueller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
